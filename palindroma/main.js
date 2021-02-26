683e154e2da47731efe7457088e50149186ab849
Emilio94 <emilio.994@gmail.com>
2021-02-26 16:33:23 +0100

    file sistemati, palindroma ancora da finire

--- palindroma/main.js
+++ palindroma/main.js
@@ -2,15 +2,15 @@
 Chiedere all'utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
-var parola = isNaN(prompt("Inserisci una parola"));
+var parola = prompt("Inserisci una parola").toLowerCase();
+if (isNaN(parola)) {
 
-function palindroma(value) {
-    if (value) {
-        // La parola letta in entrambi i sensi è la stessa parola, a+b+c = c+b+a
-
-    }
+}
+else {
+    alert("Utilizzare solo lettere")
 }
 
-
-
+function palindroma(value) {
+    
+}
 
